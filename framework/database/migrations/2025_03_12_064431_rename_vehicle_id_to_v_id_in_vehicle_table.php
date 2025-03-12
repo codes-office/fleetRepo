b4ac2e07cf02b8fd36965d4a515885b731e6690c
unknown <jmtharunchari@gmail.com>
2025-03-12 12:23:15 +0530

    some changes in migrartion model

---
+++ framework/database/migrations/2025_03_12_064431_rename_vehicle_id_to_v_id_in_vehicle_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up() {
+        Schema::table('vehicle', function (Blueprint $table) {
+            $table->renameColumn('vehicle_id', 'v_id');
+        });
+    }
+
+    public function down() {
+        Schema::table('vehicle', function (Blueprint $table) {
+            $table->renameColumn('v_id', 'vehicle_id');
+        });
+    }
+};
